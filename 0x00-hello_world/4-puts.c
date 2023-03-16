edb547c6ea86d9c5967ad6be2fbf845ae9dd39bd
EUGEN-NYONGESA <eugenyongesa@gmail.com>
2023-03-16 16:36:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Program that prints a line with the put function
+ * Returns: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
