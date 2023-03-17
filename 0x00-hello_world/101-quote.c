c45c3c6cbf52a832e2e73c71bb34802ab2a9cc61
EUGEN-NYONGESA <eugenyongesa@gmail.com>
2023-03-16 18:34:22 -0700

    8. 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <unistdio.h>
 
 /**
- * main - Entry point
+ * main - A program that prints a line to a standard error
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
